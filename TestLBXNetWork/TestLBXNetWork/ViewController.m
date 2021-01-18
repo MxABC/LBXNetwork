b7bfbd7e720e273cee0a9e264f39b7bc334fa382
MxABC <lbxia20091227@gmail.com>
2021-01-18 08:32:36 +0800

    update

--- TestLBXNetWork/TestLBXNetWork/ViewController.m
+++ TestLBXNetWork/TestLBXNetWork/ViewController.m
@@ -37,7 +37,206 @@
    
 }
 
+- (NSString*)documentDir
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docDir = [paths lastObject];
+    return docDir;
+}
+
 - (void)testFormData
+{
+    UIImage *image = [UIImage imageNamed:@"guide1"];
+    NSData *imageData =  UIImageJPEGRepresentation(image, 0.3);
+    
+    NSString* filePath = [[self documentDir]stringByAppendingPathComponent:@"1.jpg"];
+    [imageData writeToFile:filePath atomically:YES];
+    
+    LBXHttpRequest *request = [[LBXHttpRequest alloc]init];
+    request.requestUrl = @"http://192.168.1.195:8080/api/medicalaid/business/image/uploadImageData";
+    
+    //除文件外的参数
+    NSDictionary *dic = @{@"injuryNo":@"ios112",@"userCode":@"iosUserCode1"
+                          ,@"loanApplyNo":@"iosLoanAppNo",@"policyNo":@"ios",
+                          @"picType":@"12"
+    };
+    request.requestParameters = dic;
+    
+    
+//    NSArray* array = @[@{@"fileData":imageData,@"name":@"file",@"fileName":@"1.jpg",@"mimeType":@"image/jpeg"}];
+    
+    NSArray* array = @[@{@"filePath":filePath,@"name":@"file",@"fileName":@"1.jpg",@"mimeType":@"image/jpeg"}];
+
+    
+    request.requestElseParameters = array;
+    
+    request.responseAcceptableContentTypes = [NSSet setWithObjects:@"text/plain", @"text/html",@"application/json", @"text/json" ,@"text/javascript", nil];
+    
+    [LBXNetwork PostFormWithRequest:request progress:nil complection:^(LBXHttpResponse * _Nonnull response) {
+        
+        if (response.success) {
+         
+            
+            NSDictionary *dictFromData = [NSJSONSerialization JSONObjectWithData:response.responseObject
+                                                                       options:NSJSONReadingAllowFragments
+                                                                           error:nil];
+            
+            NSLog(@"%@",dictFromData);
+            
+        }
+        else if(response.error)
+        {
+            NSLog(@"error:%@",response.error);
+        }
+        
+      
+    }];
+}
+
+- (void)testFormData2
+{
+    UIImage *image = [UIImage imageNamed:@"guide1"];
+    NSData *imageData =  UIImageJPEGRepresentation(image, 0.3);
+    
+    NSString* filePath = [[self documentDir]stringByAppendingPathComponent:@"1.jpg"];
+    [imageData writeToFile:filePath atomically:YES];
+    
+
+    NSData *fileData = [NSData dataWithContentsOfFile:filePath];
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.requestSerializer.timeoutInterval = 100;
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/plain", @"text/html",@"application/json", @"text/json" ,@"text/javascript", nil];
+    
+    //除文件外的参数
+    NSDictionary *dic = @{@"injuryNo":@"ios11",@"userCode":@"iosUserCode"
+                          ,@"loanApplyNo":@"iosLoanAppNo",@"policyNo":@"ios",
+                          @"picType":@"11"
+    };
+    
+    [manager POST:@"http://192.168.1.195:8080/api/medicalaid/business/image/uploadImageData" parameters:dic headers:nil constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+        
+        [formData appendPartWithFileData:imageData
+                                    name:@"file"
+                                fileName:@"1.jpg"
+                                mimeType:@"image/jpeg"];
+//
+        
+        [formData appendPartWithFileURL:[NSURL fileURLWithPath:filePath] name:@"file" fileName:@"1.jpg" mimeType:@"image/jpeg" error:nil];
+        
+        
+//        - (BOOL)appendPartWithFileURL:(NSURL *)fileURL
+//                                 name:(NSString *)name
+//                             fileName:(NSString *)fileName
+//                             mimeType:(NSString *)mimeType
+//                                error:(NSError * _Nullable __autoreleasing *)error;
+        
+//        [formData appendPartWithFormData:imageData name:@"file"];
+        
+    } progress:^(NSProgress * _Nonnull uploadProgress) {
+     
+        NSLog(@"progress：%f,finish:%d",uploadProgress.fractionCompleted,uploadProgress.isFinished);
+        
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        NSLog(@"success:%@",responseObject);
+        
+        NSDictionary *dictFromData = [NSJSONSerialization JSONObjectWithData:responseObject
+                                                                   options:NSJSONReadingAllowFragments
+                                                                       error:nil];
+        
+        NSLog(@"%@",dictFromData);
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        NSLog(@"%@",error);
+    }];
+
+}
+
+
+
+
+
+/// 表单上传文件
+/// @param URLString 接口地址
+/// @param parameters 参数
+/// @param headers headers
+/// @param fileParameters 文件参数
+/// @param uploadProgress 上传进度
+/// @param success 接口调用成功结果返回
+/// @param failure 接口调用失败
+- (NSURLSessionDataTask*)PostFormData:(NSString *)URLString
+          parameters:(nullable id)parameters
+             headers:(nullable NSDictionary<NSString *,NSString *> *)headers
+      fileParameters:(NSArray<NSDictionary*>*)fileParameters
+            progress:(nullable void (^)(double progress))uploadProgress
+             success:(nullable void (^)(id _Nullable))success
+             failure:(void (^)(NSError * _Nonnull))failure
+{
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.requestSerializer.timeoutInterval = 100;
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/plain", @"text/html",@"application/json", @"text/json" ,@"text/javascript", nil];
+    
+
+    void (^onProgress)(NSProgress * _Nonnull uploadProgress) = nil;
+    
+    if (uploadProgress) {
+        
+        onProgress = ^(NSProgress * _Nonnull progress)
+        {
+            uploadProgress(progress.fractionCompleted);
+        };
+    }
+    
+    return [manager POST:URLString parameters:parameters headers:headers constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+
+        for (int i = 0; i < fileParameters.count; i++) {
+            NSDictionary *dic = fileParameters[i];
+            //下面2个参数有一个即可
+            NSData *fileData = dic[@"fileData"];
+            NSString *filePath = dic[@"filePath"];
+            
+            NSString *name = dic[@"name"];
+            NSString *fileName = dic[@"fileName"];
+            NSString * mimeType = dic[@"mimeType"];
+            
+            if (fileData) {
+                [formData appendPartWithFileData:fileData
+                                            name:name
+                                        fileName:fileName
+                                        mimeType:mimeType];
+            }
+            else if(filePath)
+            {
+                NSError *error = nil;
+                [formData appendPartWithFileURL:[NSURL fileURLWithPath:filePath] name:name fileName:fileName mimeType:mimeType error:&error];
+                
+                if (error && failure) {
+                    failure(error);
+                }
+            }
+        }
+        
+    } progress:onProgress success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        if (success) {
+            success(responseObject);
+        }
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        if (failure) {
+            failure(error);
+        }
+    }];
+    
+}
+
+- (void)testFormData1
 {
     UIImage *image = [UIImage imageNamed:@"guide1"];
     NSData *imageData =  UIImageJPEGRepresentation(image, 0.3);
